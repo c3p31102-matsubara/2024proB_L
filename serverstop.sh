1a8f0224f73c7fb72ebbc70c52876200ee87eff6
dynamiteOpanty <103298123+dynamiteOpanty@users.noreply.github.com>
2024-06-03 10:29:20 +0900

    mysqldとhttpdの分離、my.cnfを指定するように

--- serverstop.sh
+++ serverstop.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
-PSID=$(pstree | grep -e "httpd" | grep -e "=" | grep -v "grep" | awk '{print $2}' | sed 's/0*\([0-9]*[0-9]$\)/\1/g')
-if [[ ! -z $PSID ]]; then
-    kill $PSID
+httpd_PSID=$(pstree | grep -e "httpd" | grep -e "=" | grep -v "grep" | awk '{print $2}' | sed 's/0*\([0-9]*[0-9]$\)/\1/g')
+if [[ ! -z $httpd_PSID ]]; then
+    kill $httpd_PSID
     echo "stopped httpd running"
-    brew services stop mariadb
 else
     echo "httpd wasn't found running"
+fi
+mysqld_PSID=$(pstree | grep -e "mysqld" | grep -e "=" | grep -v "grep" | awk '{print $2}' | sed 's/0*\([0-9]*[0-9]$\)/\1/g')
+if [[ ! -z $mysqld_PSID ]]; then
+    kill $mysqld_PSID
+    echo "stopped mysqld running"
+else
+    echo "mysqld wasn't found running"
 fi
