31076eaf0f47e15873fde5c16fd570321c13a183
dynamiteOpanty <103298123+dynamiteOpanty@users.noreply.github.com>
2024-06-13 12:57:39 +0900

    区切った1

--- wp/APItest.php
+++ wp/APItest.php
@@ -8,7 +8,10 @@
 <body>
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script type="module">
-        import { Post, dataType } from "./js/APIaccessor.js";
+        <?php
+        // file_put_contents("./log.txt", "test")
+        ?>
+        import { getjson, dataType } from "./js/APIaccessor.js";
         let userlist = await getjson(dataType.managementlist, false, 1);
         console.log(userlist);
     </script>
--- wp/php/API.php
+++ wp/php/API.php
@@ -14,38 +14,45 @@ enum dataType: string
     case management = "management";
     case affiliation = "affiliation";
 }
-if (isset($_REQUEST["data"]))
-    $req_target = dataType::from($_REQUEST["data"]);
-else
-    echo [];
-if (isset($_REQUEST["id"]))
-    $req_id = $_REQUEST["id"];
-else
-    $req_id = 1;
-if (isset($_REQUEST["recursive"]))
-    $req_recr = $_REQUEST["recursive"];
-else
-    $req_recr = false;
-$dbh = connect();
-$userlist = new user_list($dbh);
-$lostitemlist = new lostitem_list($dbh);
-$discoverylist = new discovery_list($dbh);
-$managementlist = new management_list($dbh);
-$affiliationlist = new affiliation_list($dbh);
-match ($req_target) {
-    dataType::userlist => $obj = $userlist,
-    dataType::lostitemlist => $obj = $lostitemlist,
-    dataType::discoverylist => $obj = $discoverylist,
-    dataType::managementlist => $obj = $managementlist,
-    dataType::affiliationlist => $obj = $affiliationlist,
-    dataType::user => $obj = $userlist->GetContent_by_id($req_id),
-    dataType::lostitem => $obj = $lostitemlist->GetContent_by_id($req_id),
-    dataType::discovery => $obj = $discoverylist->GetContent_by_id($req_id),
-    dataType::management => $obj = $managementlist->GetContent_by_id($req_id),
-    dataType::affiliation => $obj = $affiliationlist->GetContent_by_id($req_id),
-};
-header("Content-Type: application/json; charset=utf-8");
-if ($req_recr)
-    echo $obj->Serialize_recursive();
-else
-    echo $obj->Serialize();
+if (!isset($_REQUEST["type"])) {
+    file_put_contents("./log.txt", "error1");
+    exit;
+}
+$req_type = $_REQUEST["type"];
+if ($req_type == "json") {
+    if (isset($_REQUEST["data"]))
+        $req_target = dataType::from($_REQUEST["data"]);
+    else
+        echo [];
+    if (isset($_REQUEST["id"]))
+        $req_id = $_REQUEST["id"];
+    else
+        $req_id = 1;
+    if (isset($_REQUEST["recursive"]))
+        $req_recr = $_REQUEST["recursive"];
+    else
+        $req_recr = false;
+    $dbh = connect();
+    $userlist = new user_list($dbh);
+    $lostitemlist = new lostitem_list($dbh);
+    $discoverylist = new discovery_list($dbh);
+    $managementlist = new management_list($dbh);
+    $affiliationlist = new affiliation_list($dbh);
+    match ($req_target) {
+        dataType::userlist => $obj = $userlist,
+        dataType::lostitemlist => $obj = $lostitemlist,
+        dataType::discoverylist => $obj = $discoverylist,
+        dataType::managementlist => $obj = $managementlist,
+        dataType::affiliationlist => $obj = $affiliationlist,
+        dataType::user => $obj = $userlist->GetContent_by_id($req_id),
+        dataType::lostitem => $obj = $lostitemlist->GetContent_by_id($req_id),
+        dataType::discovery => $obj = $discoverylist->GetContent_by_id($req_id),
+        dataType::management => $obj = $managementlist->GetContent_by_id($req_id),
+        dataType::affiliation => $obj = $affiliationlist->GetContent_by_id($req_id),
+    };
+    header("Content-Type: application/json; charset=utf-8");
+    if ($req_recr)
+        echo $obj->Serialize_recursive();
+    else
+        echo $obj->Serialize();
+}
