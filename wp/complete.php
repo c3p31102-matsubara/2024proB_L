b60cbcb6b112c5d700c4e93150f0eef1f48882ad
dynamiteOpanty <103298123+dynamiteOpanty@users.noreply.github.com>
2024-06-18 10:55:20 +0900

    user登録を動作させる

--- wp/complete.php
+++ wp/complete.php
@@ -36,10 +36,18 @@
       Copyright &copy; 2024,Team J,All rights reserved.
     </p>
   </footer>
+  <script>
+    const form_name = '<?php echo $_POST["name"]?>'
+    const form_affiliation = '<?php echo $_POST["affiliation"]?>'
+    const form_email = '<?php echo $_POST["email"]?>'
+    const form_telephone = '<?php echo $_POST["telephone"]?>'
+  </script>
   <script src="js/headerLoader.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
     crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script type="module" src="js/user-insert.js"></script>
 </body>
 
 </html>
---
+++ wp/js/user-insert.js
@@ -0,0 +1,25 @@
+//@ts-check
+
+// 各種変数は存在するのでエラーは無視
+// 叶うならば解決したかったが
+
+import { dataType, insert } from "./APIaccessor.js";
+window.addEventListener('load', async (event) => {
+    let affiliations = [
+        "情報学部学生",
+        "健康栄養学部学生",
+        "情報学部教員",
+        "事務局",
+        "その他"
+    ]
+    let affiliation_number = affiliations.indexOf(form_affiliation) + 1
+    console.log(affiliation_number);
+    let attribute;
+    if (affiliation_number == 1 || affiliation_number == 2) {
+        attribute = "student";
+    }
+    else {
+        attribute = "teacher";
+    }
+    insert(dataType.userlist, [attribute, "c3p31102",affiliation_number, form_email, form_telephone, form_name])
+})
